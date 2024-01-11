954d668038c726515632a4b6b78ca91573633087
optimusace <shirshakkhadka6@gmail.com>
2024-01-12 00:16:58 +0545

    'commit'

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
+//ACCESSING GAME DISPLAY SECTIONS AND PLAY BUTTONS AND SCORE
+const initialDisplay = document.getElementById("initialDisplay")
+const gameBox = document.getElementById("gameBox")
+const displayFinalScore = document.getElementById("display-final-score")
+const playBtn = document.getElementById("play-button")
+const playAgainBtn = document.getElementById("play-again-button")
+const finalScore = document.getElementById("final-score")
+
+//ADDING CLICK EVENT LISTENERS TO PLAY AND PLAY AGAIN BUTTONS
+playBtn.addEventListener("click",()=>{
+    initialDisplay.style.display = "none"
+    gameBox.style.display = "block"
+    generateAims()
+    startTimer()
+    checkHit()
+})
+
+playAgainBtn.addEventListener("click",function(){
+    displayFinalScore.style.display = "none"
+    gameBox.style.display = "block"
+    totalScore = 0
+    currentScore.textContent = totalScore 
+    finalScore.textContent = totalScore
+    generateAims()
+    startTimer()
+    checkHit()
+})
+
+//ACCESS DIFFERENT ELEMTNS TO DISPLAY HIT,SOCRE,TIMER 
 const valueToHit = document.getElementById("hitValue")
-const score = document.getElementById("scoreValue")
+const currentScore = document.getElementById("currentScore")
 const timer = document.getElementById("timerValue")
 const aimsContainer = document.getElementById("aims")
-const displayScoreContainer = document.getElementById("displayScore")
-const aim = document.getElementsByClassName("aims")
-
-gameStart = false 
-gameOver = false 
-
 
 const totalAims = 36
 let totalScore = 0
-score.textContent = totalScore
-
-/* const displayScore = ()=>{
-    const titlePara = document.createElement("p")
-    titlePara.classList.add("display-score-title")
-    titlePara.textContent = "Total Score : "
-
-    const scorePara = document.createElement("p");
-    scorePara.classList.add("display-score")
-    scorePara.textContent = totalScore
-
-    displayScoreContainer.appendChild(titlePara)
-    displayScoreContainer.appendChild(scorePara)
-
-} */
+currentScore.textContent = totalScore
 
 const startTimer = ()=>{
-    seconds = 60 
-    window.setInterval(function(){
+    seconds = 20
+    let timerInterval = window.setInterval(updateTimer,1000)
+    function updateTimer(){
         if(seconds > 0){
-            seconds -= 1
+            seconds -= 1 
             timer.textContent = seconds
+        }else{
+            gameBox.style.display = "none"
+            displayFinalScore.style.display = "block"
+            window.clearInterval(timerInterval)
         }
-    },1000)
+    }
 }
 
 const getNumberToHit = () => {
-    const number = Math.floor(Math.random()*totalAims)
+    //Math.floor(Math.random()*(max-min+1)+min)
+    const number = Math.floor(Math.random()*(totalAims-1+1)+1)
     if(number === 0){
         getNumberToHit()
     }
@@ -55,13 +70,11 @@ const getAimNumbers = () =>{
 }
 
 const generateAims = () =>{
-    //first remove old aims from the aim container
+    //REMOVING OLD AIMS FROM THE AIM CONTAINER
     while(aimsContainer.hasChildNodes()){
         aimsContainer.removeChild(aimsContainer.firstChild)
     }
-
-    //then add new aims to the aim container
-    //these aims are removed and add to increase the randomness
+    //THEN AGAIN ADD AIMS IN THE AIM CONTAINER RANDOMLY
     const aimNumbers = getAimNumbers()
     for(let i=0;i< totalAims;i++){
         const randomIndex = Math.floor(Math.random() * aimNumbers.length)
@@ -71,7 +84,6 @@ const generateAims = () =>{
         const aim = document.createElement("p")
         aim.classList.add("aim")
         aim.textContent = number
-        //aim.style.color = ""
 
         aimsContainer.appendChild(aim)
     }
@@ -83,13 +95,12 @@ const checkHit = () =>{
         const clickedNumber = parseInt(e.target.textContent)
         if(numberToHit === clickedNumber){
             totalScore+=1
-            score.textContent = totalScore
+            currentScore.textContent = totalScore
+            finalScore.textContent = totalScore
             generateAims()
             checkHit()
         }
     })
 }
 
-generateAims()
-startTimer()
-checkHit()
+
